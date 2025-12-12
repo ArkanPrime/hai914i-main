19578d7851ad8a33d98e2455ad909afec84bfb05
arkan <ramy.massal@etu.umontpellier.fr>
2025-12-12 16:37:38 +0100

    commit de tout

--- watdiv-mini-projet-partie-2/regenerate_queryset.sh
+++ watdiv-mini-projet-partie-2/regenerate_queryset.sh
@@ -1,13 +1,24 @@
-for qt in testsuite/templates/*.sparql-template;
-do
-  qt2=${qt##*templates/}
-  out="testsuite/queries/${qt2%.sparql-template}.queryset"
+#!/bin/bash
 
-  bin/Release/watdiv -q model/wsdbm-data-model.txt "$qt" 100 1 |
+# dossiers
+QUERY_DIR="testsuite/queries"
+DATA_DIR="../data"
+
+mkdir -p "$QUERY_DIR"
+mkdir -p "$DATA_DIR"
+
+# génération + déduplication
+for qt in testsuite/templates/*.sparql-template; do
+  qt2=${qt##*templates/}
+  out="$QUERY_DIR/${qt2%.sparql-template}.queryset"
+
+  echo "Génération : $qt2"
+
+  bin/Release/watdiv -q model/wsdbm-data-model.txt "$qt" 1000 1 |
   awk '
     BEGIN { RS=""; ORS="\n\n" }
     {
-      gsub(/[ \t\n]+/, " ", $0)   # normalisation
+      gsub(/[ \t\n]+/, " ", $0)
       if (!($0 in seen)) {
         seen[$0]=1
         print $0
@@ -20,3 +31,15 @@ do
     }
   ' > "$out"
 done
+
+# concaténation sans _10000
+find "$QUERY_DIR" -type f -name "*.queryset" ! -name "*_10000*" \
+  -exec cat {} + > "$DATA_DIR/all_queries.queryset"
+
+# concaténation avec _10000 uniquement
+find "$QUERY_DIR" -type f -name "*.queryset" -name "*_10000*" \
+  -exec cat {} + > "$DATA_DIR/all_queries_10000.queryset"
+
+echo "Fichiers créés :"
+echo " - data/all_queries.queryset"
+echo " - data/all_queries_10000.queryset"
