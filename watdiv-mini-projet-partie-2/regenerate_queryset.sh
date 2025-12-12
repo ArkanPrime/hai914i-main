458451d04a5286e7805d30d34cbff62e87ea0997
arkan <ramy.massal@etu.umontpellier.fr>
2025-12-12 15:59:31 +0100

    script pour enlever les doujblons

--- watdiv-mini-projet-partie-2/regenerate_queryset.sh
+++ watdiv-mini-projet-partie-2/regenerate_queryset.sh
@@ -1,5 +1,22 @@
-for qt in testsuite/templates/*.sparql-template; 
+for qt in testsuite/templates/*.sparql-template;
 do
-   qt2=${qt##*templates/}
-   bin/Release/watdiv -q model/wsdbm-data-model.txt ${qt} 1000 1 > testsuite/queries/${qt2%.sparql-template}.queryset ;
-done;
+  qt2=${qt##*templates/}
+  out="testsuite/queries/${qt2%.sparql-template}.queryset"
+
+  bin/Release/watdiv -q model/wsdbm-data-model.txt "$qt" 100 1 |
+  awk '
+    BEGIN { RS=""; ORS="\n\n" }
+    {
+      gsub(/[ \t\n]+/, " ", $0)   # normalisation
+      if (!($0 in seen)) {
+        seen[$0]=1
+        print $0
+      } else {
+        dup++
+      }
+    }
+    END {
+      print "Doublons supprimés :", dup > "/dev/stderr"
+    }
+  ' > "$out"
+done
